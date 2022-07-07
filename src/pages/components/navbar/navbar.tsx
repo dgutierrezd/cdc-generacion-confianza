43b488d6be7d59f2e5eefda95761deab2e68dc7a
“Daniel <danielgudu22@gmail.com>
2022-07-07 11:52:20 -0500

    create evolucion y resultados page

--- src/pages/components/navbar/navbar.tsx
+++ src/pages/components/navbar/navbar.tsx
@@ -1,75 +1,78 @@
-import { Box, Button, Link, Stack, Typography } from '@mui/material';
+import { Box, Button, Link, Stack, Typography } from "@mui/material";
 // import Link from 'next/link';
 
 export const Navbar = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        justifyContent: 'space-between',
+        display: "flex",
+        justifyContent: "space-between",
         px: 12.5,
         backgroundColor: (theme) => theme.palette.background.default,
         py: 2.5,
-        position: 'sticky',
+        position: "sticky",
         top: 0,
-        zIndex: 999
+        zIndex: 999,
       }}
     >
-      <Typography color='secondary' variant='h4'>
+      <Typography color="secondary" variant="h4">
         Chec
       </Typography>
 
-      <Stack direction='row' spacing={10} alignItems='center'>
+      <Stack direction="row" spacing={10} alignItems="center">
         <Link
           // href={'#course'}
-          underline='hover'
-          color='secondary'
+          underline="hover"
+          color="secondary"
           fontSize={21.19}
-          fontFamily='Open Sans'
-          lineHeight={'23.5px'}
-          alignItems='center'
-  
+          fontFamily="Open Sans"
+          lineHeight={"23.5px"}
+          alignItems="center"
         >
-          {'Generación de confianza'}
+          {"Generación de confianza"}
         </Link>
         <Link
           // href={'#buy'}
-          underline='hover'
-          color='secondary'
+          underline="hover"
+          color="secondary"
           fontSize={21.19}
-          lineHeight={'23.5px'}
-          fontFamily='Open Sans'
-          alignItems='center'
+          lineHeight={"23.5px"}
+          fontFamily="Open Sans"
+          alignItems="center"
         >
-          {'Metodología'}
+          {"Metodología"}
         </Link>
         <Link
-          // href={'#includes'}
-          underline='hover'
-          color='secondary'
+          href={"/evolucion-resultados"}
+          underline="hover"
+          color="secondary"
           fontSize={21.19}
-          lineHeight={'23.5px'}
-          fontFamily='Open Sans'
-          alignItems='center'
+          lineHeight={"23.5px"}
+          fontFamily="Open Sans"
+          alignItems="center"
         >
-          {'Evolución y Resultados'}
+          {"Evolución y Resultados"}
         </Link>
 
         <Link
           // href={'#modules'}
-          underline='hover'
-          color='secondary'
+          underline="hover"
+          color="secondary"
           fontSize={21.19}
-          lineHeight={'23.5px'}
-          fontFamily='Open Sans'
-          alignItems='center'
+          lineHeight={"23.5px"}
+          fontFamily="Open Sans"
+          alignItems="center"
         >
-          {'Medios'}
+          {"Medios"}
         </Link>
       </Stack>
 
-      <Button color='secondary'>
-        <Typography fontSize={21.19} lineHeight={'23.5px'} fontFamily='Open Sans'>
+      <Button color="secondary">
+        <Typography
+          fontSize={21.19}
+          lineHeight={"23.5px"}
+          fontFamily="Open Sans"
+        >
           Menú
         </Typography>
       </Button>
---
+++ src/pages/evolucion-resultados/index.tsx
@@ -0,0 +1,396 @@
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import { Footer } from "../components/footer/footer";
+import { Navbar } from "../components/navbar/navbar";
+
+const EvolucionResultados = () => {
+  return (
+    <Box>
+      <Navbar />
+      <Grid
+        container
+        // width='100%'
+        pt={21}
+        justifyContent="center"
+        alignItems="center"
+        paddingBottom={14.75}
+        display="flex"
+        sx={{
+          backgroundImage: `url(${"/imgs/Fondo-EYR.png"})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Box display="flex" sx={{ flexDirection: "row" }}>
+          <Typography
+            color="secondary"
+            fontSize={56.47}
+            lineHeight={"57.3px"}
+            fontFamily="VAGROUNDEDSTD"
+            fontWeight={800}
+            paddingRight={2}
+          >
+            Evolución y
+          </Typography>
+          <Typography
+            color="primary"
+            fontSize={56.47}
+            lineHeight={"57.3px"}
+            fontFamily="VAGROUNDEDSTD"
+            fontWeight={800}
+          >
+            resultados
+          </Typography>
+        </Box>
+        <Typography
+          color="primary"
+          fontSize={23}
+          fontFamily="VAGROUNDEDSTD"
+          fontWeight={800}
+          pt={6}
+        >
+          LOGROS ALCANZADOS EN LOS MUNICIPIOS DE MANIZALES Y LA DORADA
+        </Typography>
+        <Grid width={"65%"}>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -La presencia y articulación de las iniciativas comunitarias con el
+            apoyo de la empresa, aportó a la credibilidad de diferentes procesos
+            institucionales y comunitarios, y movilizó el compromiso del
+            gobierno local para el desarrollo de dichas iniciativas.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se incremento y fortaleció la oferta de procesos educativos,
+            culturales, lúdicos y participativos para los niños, jovenes,
+            adultos, lideras y organizaciones de los municipios.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Aporte al mejoramiento de la calidad de vida: en términos del
+            fortalecimiento en las capacidades de actuación de la comunidad
+            vinculada, y process de mejoramiento en términos de ingresos de
+            personas en condiciones de pobreza y vulnerabilidad a partir de la
+            experiencia base de contratación social.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se logró fortalecer los niveles organizacionales e institucionales
+            a nivel comunitario, frente al desarrollo de capacidades de gestión
+            para la obtención de recursos.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se aporto al mejoramiento de los process de información,
+            participación social y gestión para el fortalecimiento de la calidad
+            de vida de las comunidades.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se lograron generar process de promación de la diversidad y la
+            convivencia social a partir de las iniciativas organizacionales con
+            impactos en salud, organización comunitaria, participación, uso del
+            tempo libre, cuidado del medio ambiente y relacionamiento con el
+            servicio público de energía.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se lograron fortalecer procesos educativos en aspectos como cultura
+            de la legalidad, URE, derechos y deberes.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se logró mejorar la percepción de favorabilidad, familiaridad y
+            construcción de conflanza empresa/comunidad a partir del apoyo e
+            involucramiento de CHEC con acciones e iniciativas culturales,
+            artisticas, educativas y productivas.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se logró articular y optimizar recursos en terminos financieros,
+            técnicos, humanos y de conocimiento frente a las iniciativas socials
+            que operan en los territorios.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -La empresa logró posicionarse y ser reconocida por las comunidades
+            como un aliado estratégico en el desarrollo de acciones sociales.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -El acompañamiento social del proyecto ha favorecido que en los
+            sectores de influencia, los actores faciliten el involucramiento de
+            la empresa en las acciones comunitarias propias de los territorios.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -El establecimiento de alianzas con diferentes actores ha permitido
+            el desarrollo de acciones comunitarias para el posicionamiento del
+            proyecto.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -La articulación del proceso de pérdidas en sesiones de formación
+            comunitarias del proyecto, ha permitido incentivar comportamientos
+            responsables con el servicio de energía.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se ha generado una mayor sostenibilidad y reconocimiento de las
+            acciones de los actores en cada sector, debido al acompañamiento
+            permanente de la empresa.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se logró fortalecer el desarrollo de procesos comunitarios e
+            interinstitucionales de tipo productivo.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Fue posible viabilizar acciones técnicas gracias al apoyo
+            permanente de los actores involucrados en el proyecto.
+          </Typography>
+        </Grid>
+        <Typography
+          color="primary"
+          fontSize={23}
+          fontFamily="VAGROUNDEDSTD"
+          fontWeight={800}
+          pt={6}
+        >
+          LOGROS ALCANZADOS MUNICIPIO DE MARMATO
+        </Typography>
+        <Grid width={"65%"}>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -El Reconocimiento e inclusión de los intereses, inquietudes,
+            expectativas y balance de estas con los grupos de interés para la
+            creación conjunta y redefinición de los planes de intervención con
+            perspectiva socioambiental y de Generación de Confianza.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Relación con comunidades especiales: étnicamente diversas
+            (afrodescendientes e indígenas) y mineros artesanales y a pequeña
+            escala con un enfoque diferencial de derechos.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Proceso satisfactorio con 17 grupos a su vez comunitarios,
+            sociales, cívicos, institucionales y artisticos presentes y activos
+            en el municipio de Marmato con características diversas en sus
+            propósitos organizacionales, como alcance, población y zonas de
+            influencia.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -CHEC y más puntualmente el Proyecto Generación de Confianza en el
+            municipio de Marmato, fue reconocido y valorado por los grupos de
+            interés y la comunidad, que éstos impactan como un proceso
+            destacable y con sentido socio ambiental que contribuye a las
+            expectativas y a los requerimientos que hoy día desde distintas
+            dimensiones son latentes en el territorio.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Satisfactoriamente el proyecto Generación de Confianza, se extendió
+            hacia zonas que históricamente han sido desatendidas de muchas
+            maneras como la ruralidad dispersa del municipio de Marmato.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se garantizó el acceso de los grupos de interés a una información
+            abierta, precisa, correcta, oportuna y acorde a la necesidad o
+            situación con respecto al servicio de energía eléctrica.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -El proyecto impact de forma significativa, a varios actores
+            sociales, incluidos órganos gubernamentales, instituciones
+            académicas, iniciativas privadas, colectivos juveniles y otros.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Articulación en programas y proyectos de desarrollo social de
+            alcance en la comunidad local.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Apalancamiento a iniciativas comunitarias emprendidas y propuestas
+            por los grupos de interés priorizados, con las cuales se abarcan
+            dimensiones socials, socioeconómicas, comunitarias y ambientales del
+            territorio.
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize={23}
+            fontFamily="Open Sans"
+            pt={5}
+          >
+            -Se establecieron alianzas y sinergias con actores de amplía
+            influencia en el territorio, con instituciones y organizaciones
+            sociales para la actuación en espacios comunitarios y de comunidades
+            particulares y estratégicas; todo esto a partir del respeto de cada
+            actor y aprovechando sus fortalezas con un enfoque de red.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Box
+        sx={{ backgroundColor: (theme) => theme.palette.primary.main }}
+        justifyContent="center"
+        alignItems="center"
+        textAlign="center"
+        pt={8}
+        pb={7}
+      >
+        <Typography
+          color="secondary"
+          fontSize={56.47}
+          lineHeight={"57.3px"}
+          fontFamily="VAGROUNDEDSTD"
+          fontWeight={800}
+        >
+          DATOS ANUALES DEL PROYECTO
+        </Typography>
+        <Typography
+          color="secondary"
+          fontSize={56.47}
+          lineHeight={"57.3px"}
+          fontFamily="VAGROUNDEDSTD"
+          fontWeight={800}
+        >
+          GENERACIÓN DE CONFIANZA
+        </Typography>
+        <Image src={"/imgs/Tablaeyr.png"} width={1072} height={774}></Image>
+        <Typography
+          color="secondary"
+          fontSize={23}
+          fontFamily="VAGROUNDEDSTD"
+          fontWeight={800}
+          pt={7}
+        >
+          OBSERVACIÓN AÑO 2020
+        </Typography>
+        <Typography
+          color="#003c1c"
+          fontSize={23}
+          fontFamily="Open Sans"
+          pt={4}
+          px={52.5}
+          align="justify"
+        >
+          El proceso de Generación de Confianza entró en receso dada la
+          situación que está viviendo el país, en el marco de la pandemia
+          producto del COVID-19. No obstante se aprovechó el relacionamiento
+          construido a través del tiempo, con algunos actores municipales y
+          comunitarios, para viabilizar acciones y obtener información en
+          momentos requeridos por los procesos técnicos.
+        </Typography>
+      </Box>
+      <Footer />
+    </Box>
+  );
+};
+
+export default EvolucionResultados;
