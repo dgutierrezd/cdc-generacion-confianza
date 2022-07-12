e3685a67dd272051ab15998cc95424747b98f9fc
“Daniel <danielgudu22@gmail.com>
2022-07-12 09:42:47 -0500

    add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
-import { ThemeProvider } from '@mui/material';
-import { AppProps } from 'next/app';
-import theme from '../theme/theme';
+import { ThemeProvider } from "@mui/material";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import theme from "../theme/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <title>chec Generación de Confianza</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,18 @@
 // pages/_document.js
 
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-          <link rel='stylesheet' href='/font/font.css' />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link rel="stylesheet" href="/font/font.css" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap'
-            rel='stylesheet'
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap"
+            rel="stylesheet"
           />
         </Head>
         <body style={{ margin: 0 }}>
